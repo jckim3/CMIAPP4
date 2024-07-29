a071431825b3489a41ff5983bc797352188766f6
JC Kim <jckim3@gmail.com>
2024-07-29 16:26:31 -0600

    Initial Commit

---
+++ CMIAPP4/CMIAPP4App.swift
@@ -0,0 +1,17 @@
+//
+//  CMIAPP4App.swift
+//  CMIAPP4
+//
+//  Created by JC Kim on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CMIAPP4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CMIAPP4UITests/CMIAPP4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CMIAPP4UITestsLaunchTests.swift
+//  CMIAPP4UITests
+//
+//  Created by JC Kim on 7/29/24.
+//
+
+import XCTest
+
+final class CMIAPP4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
